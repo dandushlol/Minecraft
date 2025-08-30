e1d507894012ff40519e97d9e1c012189dbcf06b
GitHub Action <action@github.com>
2025-08-30 08:52:20 +0000

    Initialize template repository proper
    Set package name to com.github.dandushlol.minecraft
    Set project name to Minecraft
    Set modid to minecraft

--- src/main/java/com/github/dandushlol/minecraft/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/dandushlol/minecraft/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.dandushlol.minecraft.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
